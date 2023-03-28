4dac45850b63a1100c38641cfc4a3801c60935d0
Mateusz Kierznowski <mateusz.kierznowski@price4wheels.pl>
2023-03-28 21:05:23 +0200

    update utils that are able to process single frame

--- utils.py
+++ utils.py
@@ -221,7 +221,7 @@ def format_frames(frame, output_size):
   frame = tf.image.resize_with_pad(frame, *output_size)
   return frame
 
-def frames_from_video_file(video_path, n_frames, output_size = (224,224), frame_step = 15):
+def frames_from_video_file(video_path, n_frames, output_size = (224,224), frame_step = 1):
   """
     Creates frames from each video file present for each category.
 
@@ -250,16 +250,18 @@ def frames_from_video_file(video_path, n_frames, output_size = (224,224), frame_
   src.set(cv2.CAP_PROP_POS_FRAMES, start)
   # ret is a boolean indicating whether read was successful, frame is the image itself
   ret, frame = src.read()
-  result.append(format_frames(frame, output_size))
+  print(ret)
+  if ret:
+    result.append(format_frames(frame, output_size))
 
-  for _ in range(n_frames - 1):
-    for _ in range(frame_step):
-      ret, frame = src.read()
-    if ret:
-      frame = format_frames(frame, output_size)
-      result.append(frame)
-    else:
-      result.append(np.zeros_like(result[0]))
+    for _ in range(n_frames - 1):
+      for _ in range(frame_step):
+        ret, frame = src.read()
+      if ret:
+        frame = format_frames(frame, output_size)
+        result.append(frame)
+      else:
+        result.append(np.zeros_like(result[0]))
   src.release()
   result = np.array(result)[..., [2, 1, 0]]
 
@@ -294,8 +296,13 @@ class FrameGenerator:
       random.shuffle(pairs)
 
     for path, name in pairs:
-      video_frames = frames_from_video_file(path, self.n_frames)
+      try:
+        video_frames = frames_from_video_file(path, self.n_frames)
+      except IndexError:
+        pass
+
       label = self.class_ids_for_name[name] # Encode labels
       if self.n_frames == 1:
         video_frames = video_frames.reshape(224, 224, 3)
+      print(video_frames.shape)
       yield video_frames, label
