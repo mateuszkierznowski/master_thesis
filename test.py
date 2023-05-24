2cfdf1a7ad31f10d00ce22bc2ac975be3d30fe64
Mateusz Kierznowski <mateusz.kierznowski@price4wheels.pl>
2023-05-24 17:50:57 +0200

    update utils

--- test.py
+++ test.py
@@ -104,6 +104,7 @@ import tensorflow as tf
 from utils import *
 import pathlib
 from utils import FrameGenerator
+import matplotlib.pyplot as plt
 
 subset_paths = {'train_master': pathlib.Path(r"C:\Users\User\Desktop\Master_videos_all\train_3"),
                 'val_master': pathlib.Path(r"C:\Users\User\Desktop\Master_videos_all\val_3")}
@@ -114,45 +115,15 @@ batch_size = 24
 output_signature = (tf.TensorSpec(shape=(None, None, None, 3), dtype=tf.float32),
                     tf.TensorSpec(shape=(), dtype=tf.int16))
 
-train_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['train_master'], n_frames, augmentation=False, training=False, frame_step=1),
+train_ds_no_aug = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['train_master'], n_frames, augmentation=False, training=False, frame_step=52),
                                           output_signature=output_signature)
 
-train_ds_2 = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['train_master'], n_frames, augmentation=False,  training=False, frame_step=52),
+train_ds_aug = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['train_master'], n_frames, augmentation=True,  training=False, frame_step=52),
                                           output_signature=output_signature)
 
 
 train_gen = FrameGenerator(subset_paths['train_master'], n_frames, augmentation=True, training=False, frame_step=1)
 train_gen16 = FrameGenerator(subset_paths['train_master'], n_frames, augmentation=True, training=False, frame_step=16)
-
-
-from video_preprocessing.vidaug.vidaug import augmentors as va
-
-sometimes_05 = lambda aug: va.Sometimes(.5, aug)
-sometimes_02 = lambda aug: va.Sometimes(.2, aug)
-sometimes_01 = lambda aug: va.Sometimes(.1, aug)
-
-seq = va.Sequential([
-    va.RandomRotate(degrees=15), # randomly rotates the video with a degree randomly choosen from [-10, 10]
-    sometimes_05(va.HorizontalFlip()),
-    sometimes_01(va.PiecewiseAffineTransform()),
-    sometimes_01(va.Superpixel(10)),
-    sometimes_02(va.SomeOf([va.Add(10), va.Multiply(1.2)], N=1)),
-    sometimes_02(va.SomeOf([va.Downsample(.8), va.Upsample(2)], N=1))
-])
-
-for i in range(1000):
-    frames, label = next(train_gen())
-    frames_2, label_2 = next(train_gen16())
-
-plot_frames(frames)
-plot_frames(frames_2)
-
-# frame = tf.image.convert_image_dtype(frames[0:10], tf.float32)
-# frame = tf.image.resize_with_pad(frame, *(224, 224))
-
-frames_ = seq(np.array(frames))
-plot_frames(frames_)
-
 def plot_frames(frames):
     fig, axes = plt.subplots(nrows=3, ncols=4, figsize=(12, 9))
 
@@ -173,6 +144,35 @@ def plot_frames(frames):
 
     # Display the plot
     plt.show()
+
+
+from video_preprocessing.vidaug.vidaug import augmentors as va
+
+sometimes_05 = lambda aug: va.Sometimes(.5, aug)
+sometimes_02 = lambda aug: va.Sometimes(.2, aug)
+sometimes_01 = lambda aug: va.Sometimes(.1, aug)
+
+seq = va.Sequential([
+    va.RandomRotate(degrees=15), # randomly rotates the video with a degree randomly choosen from [-10, 10]
+    sometimes_05(va.HorizontalFlip()),
+    sometimes_01(va.PiecewiseAffineTransform()),
+    sometimes_01(va.Superpixel(10)),
+    sometimes_02(va.SomeOf([va.Add(10), va.Multiply(1.2)], N=1)),
+    sometimes_02(va.SomeOf([va.Downsample(.8), va.Upsample(2)], N=1))
+])
+
+for i in range(1000):
+    frames, label = next(iter(train_ds_no_aug))
+    frames_2, label_2 = next(iter(train_ds_aug))
+
+plot_frames(frames)
+plot_frames(frames_2)
+
+# frame = tf.image.convert_image_dtype(frames[0:10], tf.float32)
+# frame = tf.image.resize_with_pad(frame, *(224, 224))
+
+frames_ = seq(np.array(frames))
+plot_frames(frames_)
 
 import cv2
 from scikit_image.io import Video
